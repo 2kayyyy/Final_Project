98dc51f954c33e11eafc50f0c1aefad031cb2751
Aryan <aryankafle004@gmail.com>
2023-12-04 00:44:11 -0500

    fresh commit

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-window.onload = function () {
-    window.addEventListener('scroll', function () {
-        const header = document.querySelector("header");
-        if (window.pageYOffset > 100) {
-            header.classList.add('is-scrolling');
-        } else {
-            header.classList.remove('is-scrolling');
-        }
-    });
-
-    const menu_btn = document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav');
-
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-active');
-    });
-}
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
